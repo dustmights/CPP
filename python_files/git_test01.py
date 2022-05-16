ff8a6aa3706901d586b4eb41a421d0c0af063377
Ian Davis <ian.davis@gearboxsoftware.com>
2022-05-16 10:16:59 -0500

    final commit to branch

---
+++ python_files/git_test01.py
@@ -0,0 +1,17 @@
+"""
+Really, just a bunch of code to help me learn how to git
+
+"""
+import os
+from __future__ import pathlib
+
+def add_and_norm_path(file_path):
+    pass
+
+def more_code(input01=None, input02=None):
+    if input01:
+        print("oh, it's input 1")
+    elif input02:
+        print("huh, input 2 it seems")
+    else:
+        print("you didnt input anything")
--- python_files/readme.txt
+++ python_files/readme.txt
@@ -1,1 +1,2 @@
 this whole project is so old, it may be better to just toss it.
+git
